6bbad225c8be810c5d452b9e33581e00dabb30b7
fede <fede.lope@gmail.com>
2017-06-19 23:37:49 +1000

    Remove service discovery by name endpoint

--- discovery-app/src/main/java/movie/DiscoveryApp.java
+++ discovery-app/src/main/java/movie/DiscoveryApp.java
@@ -3,8 +3,6 @@ package movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
@@ -22,18 +18,11 @@ public class DiscoveryApp {
 
     @Autowired
     private RestTemplate restTemplate;
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @GetMapping("/topgrossing/{year}")
     String getTopGrossingMovie(@PathVariable Integer year) {
         String url = "//resilient-movie-app/movies/" + year;
         return restTemplate.getForObject(url, String.class);
-    }
-
-    @GetMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
-        return discoveryClient.getInstances(applicationName);
     }
 
     @Bean
