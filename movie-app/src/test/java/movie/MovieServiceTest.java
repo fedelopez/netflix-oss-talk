dad23b43b0e844fe69c86612e612316d0d5670c2
fede <fede.lope@gmail.com>
2017-06-19 22:41:35 +1000

    Add static import

--- movie-app/src/test/java/movie/MovieServiceTest.java
+++ movie-app/src/test/java/movie/MovieServiceTest.java
@@ -1,11 +1,12 @@
 package movie;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {MovieService.class})
@@ -16,16 +17,16 @@ public class MovieServiceTest {
 
     @Test
     public void returnsHighestGrossingMovie() throws Exception {
-        Assert.assertEquals("Finding Dory", movieService.getTopGrossingMovie(2016));
-        Assert.assertEquals("The Force Awakens", movieService.getTopGrossingMovie(2015));
-        Assert.assertEquals("Guardians of the Galaxy", movieService.getTopGrossingMovie(2014));
-        Assert.assertEquals("Iron Man 3", movieService.getTopGrossingMovie(2013));
-        Assert.assertEquals("The Avengers", movieService.getTopGrossingMovie(2012));
-        Assert.assertEquals("Harry Potter and the Deathly Hallows Part 2", movieService.getTopGrossingMovie(2011));
+        assertEquals("Finding Dory", movieService.getTopGrossingMovie(2016));
+        assertEquals("The Force Awakens", movieService.getTopGrossingMovie(2015));
+        assertEquals("Guardians of the Galaxy", movieService.getTopGrossingMovie(2014));
+        assertEquals("Iron Man 3", movieService.getTopGrossingMovie(2013));
+        assertEquals("The Avengers", movieService.getTopGrossingMovie(2012));
+        assertEquals("Harry Potter and the Deathly Hallows Part 2", movieService.getTopGrossingMovie(2011));
     }
 
     @Test
     public void returnsFallback() throws Exception {
-        Assert.assertEquals("Beauty and the Beast", movieService.getTopGrossingMovie(1984));
+        assertEquals("Beauty and the Beast", movieService.getTopGrossingMovie(1984));
     }
 }
